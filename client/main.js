3671107c32223ad46aa3e2d0b199711d0a3ab7ff
Azari <itsazari9321@gmail.com>
2022-04-16 21:51:56 -0400

    VersionWithBetterget

--- client/main.js
+++ client/main.js
@@ -1,19 +1,17 @@
+
+
 const cl = (TheThingYouWantToConsoleLog) => {console.log(TheThingYouWantToConsoleLog)}
-
-
-
-
 document.getElementById("complimentButton").onclick = function () {
     axios.get("http://localhost:4000/api/compliment/")
         .then(function (response) {
           const data = response.data;
           alert(data);
         });
-  };
+  };// orginal get 
 
 InspireButton = document.getElementById('inspire')
-Congrats = document.getElementById('Congrats')
-MakeYourOwn = document.getElementById('Diy')
+let x = 0
+let PartO = document.getElementById('wholePage')
 
 InspireButton.addEventListener('click' , () => {
      axios.get("http://localhost:4000/api/inspire/")
@@ -24,27 +22,55 @@ InspireButton.addEventListener('click' , () => {
                 } 
                 )
 }
+
 )
 
-Congrats.addEventListener('click', () => {
-    axios.get('http://localhost:4000/api/Congrats/')
-    .then(function (response) {
 
-    window.alert(response.data)
-         }
-    )
-
+AddingConted = (res,num) => { let text = document.createElement("div")
+text.innerHTML = `<h2>Name: ${res.data[num].name}</h2>
+               <p>Has a Straw? ${res.data[num].HasStraw} </p>
+               <p>How fat is this cup? ${res.data[num].radius} </p>
+               <p>How long is this cup ${res.data[num].length} inches </p>
+               <p>Color: ${res.data[num].color} </p>`
+               CupsStuff.appendChild(Text)}
+let CupsStuff = document.getElementById('CupStuff')
+// New Advanced Get 
+let correct = document.getElementById('Correct')
+correct.textContent
+const password = document.getElementById('PswInput')
+const SubmitButton = document.getElementById('StButton')
+SubmitButton.addEventListener('click', (e) => {e.preventDefault()
+     
+     if((password.value).toLowerCase() === 'joely'){
+          axios.get(`http://localhost:4000/api/Cups/`)
+               .then((res, req) => {console.log(res.data)
+                    console.log(res.data[0])
+               console.log(res.data.length)
+               console.log(text.innerHTML)
+               for(let i = 0; i < res.data.length;i++){AddingConted(res, i)}
+    
 
 })
-MakeYourOwn.addEventListener('click', () => {
-    axios.get('http://localhost:4000/api/Diy/')
-    .then(function (response) {
+          
+          
+          
+          
+          
+          
+          cl('thingshappen')
+     correct.textContent = ''
+     PartO.classList.add('clear')}
+     else if((password.value).toLowerCase() != 'joely'){x = x + 1
+     
+     if(x === 1) {correct.textContent = 'Wrong Password'}
+     else if(x > 1 && x <= 5){correct.textContent = `You've entered the wrong password ${x} times`}
+     else if(x > 5 && x < 11){correct.textContent = `${x} Too Many ATTEMPTS`}
+     else if(x === 11){correct.textContent = "Okay Ill put it for you" , password.value = 'Joely'}
+     
+     }
+     
+     
+}
 
-    window.alert(response.data)
-         }
-    )
 
-
-})
-
-
+)
--- server/index.js
+++ server/index.js
@@ -2,19 +2,15 @@ const cl = (TheThingYouWantToConsoleLog) => {console.log(TheThingYouWantToConsol
 
 const express = require("express");
 const cors = require("cors");
+const CupsFile = require('./Cups.json')
 const app = express();
 app.use(cors());
 app.use(express.json());// When we want to be able to accept JSON.
 
-
-
-
-
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+					 "Your Javascript skills are stellar.",];
   
   // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
@@ -23,10 +19,8 @@ app.get("/api/compliment", (req, res) => {
 
   res.status(200).send(randomCompliment);
   
-});
-
-
- 
+}
+); //original get
 app.get("/api/inspire", (required, res) => {
           let InspireingThing = ['You Did This Part Well', 'Im Glad you made it this far' , 
           'Nothing could be said', `Youve Come this far you can go further` , `Goodluck`, 
@@ -34,36 +28,32 @@ app.get("/api/inspire", (required, res) => {
           let rN = Math.floor(Math.random() * InspireingThing.length)
           res.status(200).send(InspireingThing[rN])
 }
-)
-
-app.get("/api/Congrats", (required, res) => {
-  let InspireingThing = ['You Did This Part Well', 'Im Glad you made it this far' , 
-  'Nothing could be said', `Youve Come this far you can go further` , `Goodluck`, 
-  `Goodluck with the rest with the rest of this project lol`]
-  let rN = Math.floor(Math.random() * InspireingThing.length)
-  res.status(200).send(InspireingThing[rN])
-
+) // new get  
+// needs a delete /* Thinking about making the delete button for all the stuff up there, so like it deletes the compliment functionality and changes the page completely  */ 
+// need a put 
+// need a post 
+app.get('/api/Cups', (require, res) => {
 
   
+ res.status(200).send(CupsFile)
+})
+
+
+
+
+AddingCups  = (req, res) => {
+  let {name, HasStraw, length , color} = req.body
+  let newCup = {
+    name,
+    HasStraw,
+    length,
+    color,
+    Id
+  }
+CupsFile.push(newCup)
+res.status(200).send(CupsFile)
 }
-)
-
-app.get("/api/Diy", (required, res) => {
-  let InspireingThing = ['You Did This Part Well', 'Im Glad you made it this far' , 
-  'Nothing could be said', `Youve Come this far you can go further` , `Goodluck`, 
-  `Goodluck with the rest with the rest of this project lol`]
-  let rN = Math.floor(Math.random() * InspireingThing.length)
-  res.status(200).send(InspireingThing[rN])
-
-
-  
-}
-)
-
-
-
-
-
+app.post('/api/Cups' , AddingCups)
 
 
 
